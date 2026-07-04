38672a5d0496d7f2f40588260914b2d09a3d3de1
dinukachamod17 <dclakshan17@gmail.com>
2026-07-04 22:23:43 +0530

    Comment out initial variable declarations and console logs for clarity

--- js/app.js
+++ js/app.js
@@ -1,9 +1,63 @@
-let name = "dinuka";
-let age = 20;
+// let name = "dinuka";
+// let age = 20;
 
-name = "chamod";
+// name = "chamod";
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
-alert("hello icet")
+// alert("hello icet")
+
+// let name = "dinuka"
+// let age = 15
+// let isAfterAl = false
+// let customers = []
+// let customer = {}
+// let salary = 7500.0
+
+// console.log(typeof salary);
+
+//--------------------------------------------
+
+// let x = 10
+// let y = "10"
+
+// console.log(x===y);
+
+//---------------------------------------------
+
+// let x = 10;
+// let y = 20;
+
+
+
+// if (x > y) {
+//     console.log("x is greater than y");
+    
+// } else if (x < y) {
+//     console.log("x is less than y");
+// }else{
+//     console.log("x is equal to y");
+    
+// }
+
+//----------------------------------------------------------
+
+let day = 5;
+
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    default:
+        console.log("Invalid Input");
+        break;
+}
+
+//-------------------------------------------------------------------
