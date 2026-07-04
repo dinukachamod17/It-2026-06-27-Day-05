234f53c478317a795dab9f597414d9cc38e8974b
dinukachamod17 <dclakshan17@gmail.com>
2026-07-04 22:25:19 +0530

    Uncomment variable declarations and console log for functionality

--- js/app.js
+++ js/app.js
@@ -19,27 +19,27 @@
 
 //--------------------------------------------
 
-// let x = 10
-// let y = "10"
+let x = 10
+let y = "10"
 
-// console.log(x===y);
+console.log(x===y);
 
 //---------------------------------------------
 
-let x = 10;
-let y = 20;
+// let x = 10;
+// let y = 20;
 
 
 
-if (x > y) {
-    console.log("x is greater than y");
+// if (x > y) {
+//     console.log("x is greater than y");
     
-} else if (x < y) {
-    console.log("x is less than y");
-}else{
-    console.log("x is equal to y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// }else{
+//     console.log("x is equal to y");
     
-}
+// }
 
 //----------------------------------------------------------
 
